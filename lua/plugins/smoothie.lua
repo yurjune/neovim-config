b4427fc8d0241feca765c01edca5f0795279a909
yurjune <yurjune@naver.com>
2025-05-12 01:59:08 +0900

    add vim-smoothie plugin

---
+++ lua/plugins/smoothie.lua
@@ -0,0 +1,13 @@
+-- Scroll animation plugin
+return {
+  "psliwka/vim-smoothie",
+  config = function()
+    vim.g.smoothie_enabled = 1
+    vim.g.smoothie_speed_exponentiation_factor = 1 -- if 1, linear easing
+
+    vim.g.smoothie_update_interval = 5
+    vim.g.smoothie_base_speed = 30
+    vim.g.smoothie_speed_constant_factor = 50
+    vim.g.smoothie_speed_linear_factor = 50
+  end,
+}
