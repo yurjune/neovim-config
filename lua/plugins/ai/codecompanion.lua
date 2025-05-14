08b143e025021da463454c33225a22109ecf3d04
yurjune <yurjune@naver.com>
2025-05-14 13:02:07 +0900

    add render-markdown to codecompanion

--- lua/plugins/ai/codecompanion.lua
+++ lua/plugins/ai/codecompanion.lua
@@ -4,6 +4,10 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      ft = { "markdown", "codecompanion" },
+    },
   },
   config = function()
     local function execute_cmd(cmd)
