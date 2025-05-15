307f8e6858680fdde94e956894d8a3302c439a9b
yurjune <yurjune@naver.com>
2025-05-16 00:35:17 +0900

    terminal to normal mode keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " " -- bind spacebar to leader key
 
 vim.keymap.set("n", "<leader>rq", ":cexpr [] | cclose<CR>", { desc = "Reset and close quick-fix" })
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, desc = "change to normal mode" })
