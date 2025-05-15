c9d2a2c8f31d4fc70fd21de07b166ab562d8c18f
yurjune <yurjune@naver.com>
2025-05-16 00:48:11 +0900

    update toggleterm config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -7,21 +7,23 @@ return {
     version = "*",
     config = function()
       require("toggleterm").setup({
-        size = 16,
-
-        open_mapping = [[<c-`>]], -- 숫자 + 단축키로 여러 터미널 세션을 관리할 수 있다.
-        insert_mappings = true, -- 노말 모드에서도 토글이 작동하게 할지 여부
-        terminal_mappings = true,
+        size = 22,
+        -- 숫자 + 단축키로 여러 터미널 세션을 관리할 수 있다.
+        -- open_mapping = [[<c-`>]],
+        open_mapping = [[<D-j>]],
         start_in_insert = true,
-
+        insert_mappings = true, -- whether or not the open mapping applies in insert mode
+        terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
+        auto_scroll = true, -- automatically scroll to the bottom on terminal output
         persist_size = true,
-        persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
+        persist_mode = false, -- if set to true (default) the previous terminal mode will be remembered
 
-        direction = "horizontal", -- 'vertical' | 'horizontal' | 'tab' | 'float'
-
+        direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float'
         -- This field is only relevant if direction is set to 'float'
         float_opts = {
-          border = "curved",
+          border = "curved", -- 'single' | 'double' | 'shadow' | 'curved'
+          width = 200,
+          height = 45,
         },
       })
     end,
