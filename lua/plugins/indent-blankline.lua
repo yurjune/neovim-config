7b2da4aae9c2fa107b5a14c7a88090c33631780b
yurjune <yurjune@naver.com>
2025-05-19 23:26:34 +0900

    indent-blankline scope config

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -29,11 +29,15 @@ return {
       enabled = true,
       debounce = 100,
       indent = {
-        -- char = "¦",
-        char = "│",
+        char = "¦",
+        -- char = "│",
         -- highlight = highlight,
         smart_indent_cap = true, -- 과도한 들여쓰기 수준에서 시각적 혼란을 감소
         repeat_linebreak = true, -- 줄 바꿈 시 들여쓰기 표시를 반복할지 여부를 결정
+      },
+      scope = {
+        show_start = false, -- 현재 스코프에 대한 밑줄 표시 여부
+        show_exact_scope = false,
       },
     })
   end,
