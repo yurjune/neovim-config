aeaf88218feb3fe3b452d92129fcb1fe0ff3b0ac
yurjune <yurjune@naver.com>
2025-05-21 22:14:22 +0900

    add keymap: focus on floating window

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,3 +2,15 @@ vim.g.mapleader = " " -- bind spacebar to leader key
 
 vim.keymap.set("n", "<leader>rq", ":cexpr [] | cclose<CR>", { desc = "Reset and close quick-fix" })
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, desc = "change to normal mode" })
+
+vim.keymap.set("n", "gh", function()
+  local windows = vim.api.nvim_tabpage_list_wins(0)
+  for _, win in ipairs(windows) do
+    local config = vim.api.nvim_win_get_config(win)
+    -- Check if the window is a floating window, since normal windows won't have a relative property
+    if config.relative ~= "" then
+      vim.api.nvim_set_current_win(win)
+      return
+    end
+  end
+end, { desc = "Focus on floating window" })
