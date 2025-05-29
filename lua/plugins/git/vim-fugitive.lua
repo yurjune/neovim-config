9a160b81825c44f09383f29ab128caa001d1ebed
yurjune <yurjune@naver.com>
2025-05-30 02:50:17 +0900

    add vim-fugitive keymaps

--- lua/plugins/git/vim-fugitive.lua
+++ lua/plugins/git/vim-fugitive.lua
@@ -1,6 +1,9 @@
 return {
-  {
-    "tpope/vim-fugitive",
-    event = "VeryLazy",
-  },
+  "tpope/vim-fugitive",
+  event = "VeryLazy",
+  config = function()
+    vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiffsplit<CR>", { desc = "Git diff" })
+    vim.keymap.set("n", "<leader>gD", "<cmd>Gvdiffsplit HEAD~1<CR>", { desc = "Git diff HEAD~1" })
+    vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<CR>", { desc = "Git blame" })
+  end,
 }
