e0a8f61f73840f42e74af6c20a45794051e15c7d
yurjune <yurjune@naver.com>
2025-06-01 17:04:14 +0900

    use meta key in vim-tmux-navigator

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -9,10 +9,10 @@ return {
     "TmuxNavigatorProcessList",
   },
   keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    { "<M-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<M-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<M-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<M-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<M-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
   },
 }
