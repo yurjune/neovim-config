3925ab0374114bb4ea3c3afe49ab2310d737e494
yurjune <yurjune@naver.com>
2025-06-01 20:41:02 +0900

    remap C-6

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,8 @@
 vim.g.mapleader = " " -- bind spacebar to leader key
 
+vim.keymap.set("n", "<C-q>", "<C-6>", { desc = "Switch to last accessed buffer" })
+vim.keymap.set("n", "<C-6>", "<Nop>")
+
 vim.keymap.set("n", "<leader>rq", ":cexpr [] | cclose<CR>", { desc = "Reset and close quick-fix" })
 -- vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, desc = "change to normal mode" })
 
