767453a4b450127b9fe705c254b92b52beef73d1
yurjune <yurjune@naver.com>
2025-06-06 15:49:43 +0900

    update vim-smoothie config

--- lua/plugins/smoothie.lua
+++ lua/plugins/smoothie.lua
@@ -3,11 +3,14 @@ return {
   "psliwka/vim-smoothie",
   config = function()
     vim.g.smoothie_enabled = vim.g.neovide and 0 or 1
+    vim.g.smoothie_update_interval = 20 -- update screen every n(ms), default 20
 
-    vim.g.smoothie_speed_exponentiation_factor = 1 -- if 1, linear easing
-    vim.g.smoothie_update_interval = 5
-    vim.g.smoothie_base_speed = 30
-    vim.g.smoothie_speed_constant_factor = 50
-    vim.g.smoothie_speed_linear_factor = 50
+    -- ex) total_time = constant_factor + (linear_factor * distance)
+    vim.g.smoothie_speed_constant_factor = 20 -- default 10
+    vim.g.smoothie_speed_linear_factor = 20 -- default 10
+
+    -- Generally should be less or equal to 1.0. (1.0 = linear easing)
+    -- Lower values produce longer but perceivably smoother animation.
+    vim.g.smoothie_speed_exponentiation_factor = 0.9 -- default 0.9
   end,
 }
