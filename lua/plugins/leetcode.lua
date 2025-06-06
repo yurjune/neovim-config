b5fee72e319bb621a40c3a2e482a6583532b3cc5
yurjune <yurjune@naver.com>
2025-06-07 01:52:08 +0900

    make leetcode.nvim use telescope picker

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -50,19 +50,74 @@ return {
   },
 
   config = function(_, opts)
-    require("leetcode").setup(opts)
+    local leetcode = require("leetcode")
+    local pickers = require("telescope.pickers")
+    local finders = require("telescope.finders")
+    local actions = require("telescope.actions")
+    local action_state = require("telescope.actions.state")
+    local conf = require("telescope.config")
 
-    vim.keymap.set("n", "<leader>lr", "<cmd>Leet run<CR>", { desc = "Run Leetcode Testcase" })
+    leetcode.setup(opts)
+
+    vim.keymap.set("n", "<leader>lt", "<cmd>Leet run<CR>", { desc = "Run Leetcode Testcase" })
     vim.keymap.set("n", "<leader>lc", "<cmd>Leet console<CR>", { desc = "Open Leetcode console" })
-    vim.keymap.set("n", "<leader>ls", "<cmd>Leet submit<CR>", { desc = "Submit Leetcode answer" })
-    vim.keymap.set("n", "<leader>lls", "<cmd>Leet last_submit<CR>", { desc = "Load Leetcode last submit" })
+    vim.keymap.set("n", "<leader>lS", "<cmd>Leet submit<CR>", { desc = "Submit Leetcode answer" })
+    vim.keymap.set("n", "<leader>lL", "<cmd>Leet last_submit<CR>", { desc = "Load Leetcode last submit" })
 
-    vim.keymap.set("n", "<leader>lel", function()
-      vim.api.nvim_command("Leet list difficulty=easy")
-    end, { desc = "Leetcode problem list - easy" })
+    local function make_difficulty_picker(title, difficulties)
+      pickers
+        .new({}, {
+          prompt_title = title,
+          finder = finders.new_table({
+            results = difficulties,
+            entry_maker = function(entry)
+              return {
+                value = entry,
+                display = entry.name,
+                ordinal = entry.name, -- 사용자 입력에 대한 필터링 기준
+              }
+            end,
+          }),
+          layout_config = {
+            width = 0.2,
+            height = 0.2,
+          },
+          sorter = conf.values.generic_sorter({}),
+          attach_mappings = function(prompt_bufnr)
+            -- replace default enter action with custom action
+            actions.select_default:replace(function()
+              actions.close(prompt_bufnr)
+              local selection = action_state.get_selected_entry()
+              if selection then
+                vim.api.nvim_command(selection.value.cmd)
+              end
+            end)
+            return true
+          end,
+        })
+        :find() -- trigger picker ui
+    end
 
-    vim.keymap.set("n", "<leader>ler", function()
-      vim.api.nvim_command("Leet random difficulty=easy")
-    end, { desc = "Leetcode random problem - easy" })
+    vim.keymap.set("n", "<leader>ll", function()
+      local difficulties = {
+        { name = "all", cmd = "Leet list" },
+        { name = "easy", cmd = "Leet list difficulty=easy" },
+        { name = "medium", cmd = "Leet list difficulty=medium" },
+        { name = "hard", cmd = "Leet list difficulty=hard" },
+      }
+
+      make_difficulty_picker("Leetcode problem list", difficulties)
+    end, { desc = "Leetcode problem list" })
+
+    vim.keymap.set("n", "<leader>lr", function()
+      local difficulties = {
+        { name = "all", cmd = "Leet random" },
+        { name = "easy", cmd = "Leet random difficulty=easy" },
+        { name = "medium", cmd = "Leet random difficulty=medium" },
+        { name = "hard", cmd = "Leet random difficulty=hard" },
+      }
+
+      make_difficulty_picker("Leetcode random problem", difficulties)
+    end, { desc = "Leetcode random problem" })
   end,
 }
