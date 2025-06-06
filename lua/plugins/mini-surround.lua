f0f9d13d7cb38df82f05d7919f59235d05ef1d5a
yurjune <yurjune@naver.com>
2025-06-07 00:33:40 +0900

    add mini.surround missing mappings

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -6,6 +6,11 @@ return {
     mappings = {
       add = "Zsa", -- Add surrounding in Normal and Visual modes
       delete = "Zsd", -- Delete surrounding
+      find = "Zsf",
+      find_left = "ZsF",
+      replace = "Zsr",
+      highlight = "Zsh",
+      update_n_lines = "Zsn",
     },
   },
 }
