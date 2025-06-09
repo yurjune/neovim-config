1193b7fa80fa241009468c8d81af6df09763899b
yurjune <yurjune@naver.com>
2025-06-09 14:09:42 +0900

    keep fold state

--- lua/core/cmd.lua
+++ lua/core/cmd.lua
@@ -7,3 +7,22 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:remove({ "o" })
   end,
 })
+
+-- for keep fold state
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  pattern = "*",
+  callback = function()
+    -- exclude empty buffer and special buffers(like help)
+    if vim.bo.filetype ~= "" and vim.bo.buftype == "" then
+      vim.cmd("silent! mkview")
+    end
+  end,
+})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.filetype ~= "" and vim.bo.buftype == "" then
+      vim.cmd("silent! loadview")
+    end
+  end,
+})
