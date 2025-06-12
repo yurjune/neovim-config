841d721e8fc4a2b7f64b3aa85d737c86bdda75ed
yurjune <yurjune@naver.com>
2025-06-13 02:18:44 +0900

    lualine: switch y and z section

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -33,8 +33,8 @@ return {
           },
           { "filetype" },
         },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
+        lualine_y = { "location" },
+        lualine_z = { "progress" },
       },
       inactive_sections = {
         lualine_a = {},
@@ -57,8 +57,8 @@ return {
                 color = { fg = "#ff9e64" },
               },
             },
-            lualine_y = { "progress" },
-            lualine_z = { "location" },
+            lualine_y = { "location" },
+            lualine_z = { "progress" },
           },
           inactive_sections = {
             lualine_b = { codecompanion_lualine.get_adapter_name },
