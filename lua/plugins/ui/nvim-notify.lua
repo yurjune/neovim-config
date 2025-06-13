348196b26a30e18cf99b3d4f1c4ddf7f37da90d8
yurjune <yurjune@naver.com>
2025-06-13 23:01:20 +0900

    nvim-notify: update settings

--- lua/plugins/ui/nvim-notify.lua
+++ lua/plugins/ui/nvim-notify.lua
@@ -6,15 +6,16 @@ return {
     local notify = require("notify")
 
     notify.setup({
+      background_colour = vim.g.colors.bg,
       fps = 60,
-      max_width = 80,
-      timeout = 2000,
+      max_width = 50,
+      minimum_width = 50,
+      max_height = 24,
+      timeout = 500,
       merge_duplicates = false,
 
       render = "wrapped-compact", -- default, minimal, simple, compact, wrapped-compact
-      background_colour = vim.g.colors.bg,
-
-      stages = "fade_in_slide_out", -- fade, slide, fade_in_slide_out, static
+      stages = "fade", -- fade, slide, fade_in_slide_out, static
       top_down = true, -- If true, notification position will be top
 
       icons = {
