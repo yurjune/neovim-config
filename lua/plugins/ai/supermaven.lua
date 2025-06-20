c065a9ff73e820537a8c3a6f6327d1f67850783d
yurjune <yurjune@naver.com>
2025-06-21 04:10:39 +0900

    supermaven: update configs

--- lua/plugins/ai/supermaven.lua
+++ lua/plugins/ai/supermaven.lua
@@ -1,14 +1,17 @@
 return {
   "supermaven-inc/supermaven-nvim",
-  enabled = false,
+  enabled = true,
   config = function()
-    require("supermaven-nvim").setup({
+    local sp = require("supermaven-nvim")
+    local api = require("supermaven-nvim.api")
+
+    sp.setup({
       keymaps = {
-        accept_suggestion = "<S-CR>",
+        accept_suggestion = "<Tab>",
         clear_suggestion = "<C-]>",
         accept_word = "<C-j>",
       },
-      ignore_filetypes = { cpp = true }, -- or { "cpp", }
+      ignore_filetypes = { "markdown" }, -- or { "cpp", }
       color = {
         -- suggestion_color = ""
         cterm = 244,
@@ -17,5 +20,14 @@ return {
       disable_inline_completion = false, -- disables inline completion for use with cmp
       disable_keymaps = false, -- disables built in keymaps for more manual control
     })
+
+    vim.keymap.set("n", "<leader>ct", function()
+      if api.is_running() then
+        vim.notify("Supermaven disabled", vim.log.levels.INFO, { title = "Supermaven" })
+      else
+        vim.notify("Supermaven enabled", vim.log.levels.INFO, { title = "Supermaven" })
+      end
+      api.toggle()
+    end, { desc = "Toggle Supermaven" })
   end,
 }
