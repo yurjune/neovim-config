ab16b507a32b507e81441023463452e26f7ccea1
yurjune <yurjune@naver.com>
2025-10-04 03:49:35 +0900

    usercmd: add save and quit alias

--- lua/core/usercmd.lua
+++ lua/core/usercmd.lua
@@ -1,7 +1,10 @@
 vim.api.nvim_create_user_command("W", "w", { desc = "Save current file" })
 vim.api.nvim_create_user_command("Wq", "wq", { desc = "Save and quit current file" })
+vim.api.nvim_create_user_command("WQ", "wq", { desc = "Save and quit current file" })
+
 vim.api.nvim_create_user_command("Q", "q", { desc = "Quit current file" })
 vim.api.nvim_create_user_command("Qa", "qa", { desc = "Quit all files" })
+vim.api.nvim_create_user_command("QA", "qa", { desc = "Quit all files" })
 
 vim.api.nvim_create_user_command("NodeCurrentFile", function()
   local file_path = vim.fn.expand("%")
