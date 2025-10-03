be9e5a323cb7b45ad167b7bd726e0f293caf5eb3
yurjune <yurjune@naver.com>
2025-10-03 22:31:00 +0900

    refactor: move diagnostics settings into a separate file

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -85,30 +85,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
-local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-vim.diagnostic.config({
-  underline = true,
-  update_in_insert = true,
-  severity_sort = true,
-  virtual_text = {
-    prefix = "■",
-    spacing = 4,
-    source = "if_many",
-  },
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = signs.Error,
-      [vim.diagnostic.severity.WARN] = signs.Warn,
-      [vim.diagnostic.severity.INFO] = signs.Info,
-      [vim.diagnostic.severity.HINT] = signs.Hint,
-    },
-  },
-  float = {
-    border = "rounded",
-    winhighlight = "Normal:DiagnosticFloat,FloatBorder:DiagnosticBorder",
-  },
-})
-
 -- Activate LSP completion
 vim.api.nvim_create_autocmd("LspAttach", {
   group = lsp_group,
