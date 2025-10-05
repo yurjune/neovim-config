2e60067b9c323fc1abf491ddf5e2a3e86eda9e30
yurjune <yurjune@naver.com>
2025-10-06 03:15:11 +0900

    mason-lspconfig: remove package

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -3,15 +3,10 @@ return {
   "williamboman/mason.nvim",
   version = "v1.11.0",
   dependencies = {
-    {
-      "williamboman/mason-lspconfig.nvim",
-      version = "v1.32.0",
-    },
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
     local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
     local mason_tool_installer = require("mason-tool-installer")
 
     -- enable mason and configure icons
@@ -22,22 +17,6 @@ return {
           package_pending = "➜",
           package_uninstalled = "✗",
         },
-      },
-    })
-
-    mason_lspconfig.setup({
-      automatic_installation = true,
-      ensure_installed = {
-        "ts_ls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "emmet_ls",
-        "pyright",
-        "rust_analyzer",
-        "marksman",
       },
     })
 
