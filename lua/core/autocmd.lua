7c6ab340ab30e2d350be7cc2fb33d936e6fdd0eb
yurjune <yurjune@naver.com>
2025-10-07 16:59:45 +0900

    autocmd: highlight yanking text

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -8,6 +8,14 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- To keep fold state
 vim.api.nvim_create_autocmd("BufWinLeave", {
   pattern = "*",
