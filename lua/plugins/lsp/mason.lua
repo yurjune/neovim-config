ae0d22a126a3c6cdb18b801d7fe2336f48d0dfce
yurjune <yurjune@naver.com>
2025-10-07 15:50:55 +0900

    mason: update package to 2.1.0

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,7 +1,6 @@
 -- A plugin that allows you to easily manage external editor tooling such as LSP servers, DAP servers, linters, and formatters through a single interface
 return {
   "williamboman/mason.nvim",
-  version = "v1.11.0",
   dependencies = {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
