bc31564e28ecc2ecdcd664b5c4c80dae99638a2e
yurjune <yurjune@naver.com>
2025-10-07 14:56:16 +0900

    maximizer: change keymap desc

--- lua/plugins/workflow/maximizer.lua
+++ lua/plugins/workflow/maximizer.lua
@@ -2,6 +2,6 @@ return {
   "szw/vim-maximizer",
   event = "VeryLazy",
   config = function()
-    vim.keymap.set("n", "<leader>wm", "<cmd>MaximizerToggle!<CR>", { desc = "Maximize screen" })
+    vim.keymap.set("n", "<leader>wm", "<cmd>MaximizerToggle!<CR>", { desc = "Maximize window fully" })
   end,
 }
