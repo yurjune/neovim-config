16fb9bd9112c5150222c1abc8deffe2fbb09bcf7
yurjune <yurjune@naver.com>
2025-10-07 20:33:27 +0900

    markview: add preview toggle keymap

--- lua/plugins/markdown/markview.lua
+++ lua/plugins/markdown/markview.lua
@@ -2,4 +2,10 @@ return {
   "OXY2DEV/markview.nvim",
   lazy = false,
   enabled = true,
+  config = function(_, opts)
+    local markview = require("markview")
+    markview.setup(opts)
+
+    vim.keymap.set("n", "<leader>mt", "<cmd>Markview toggle<cr>", { desc = "Toggle markdown preview" })
+  end,
 }
