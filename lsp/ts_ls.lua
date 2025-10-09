97ec8e52a4f53ed6fe606dc49978c46555726cc8
yurjune <yurjune@naver.com>
2025-10-09 18:09:59 +0900

    lsp: polish ts_ls lsp config

--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -1,7 +1,20 @@
 return {
-  cmd = { "typescript-language-server", "--stdio" },
-  filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
-  root_markers = { "package.json", "tsconfig.json", "jsconfig.json", ".git" },
+  cmd = {
+    "typescript-language-server",
+    "--stdio",
+  },
+  filetypes = {
+    "typescript",
+    "javascript",
+    "typescriptreact",
+    "javascriptreact",
+  },
+  root_markers = {
+    "package.json",
+    "tsconfig.json",
+    "jsconfig.json",
+    ".git",
+  },
   init_options = {
     preferences = {
       providePrefixAndSuffixTextForRename = false,
