fe326d73b801517ba15cf6903ca843c7840d41f6
yurjune <yurjune@naver.com>
2025-10-10 01:44:47 +0900

    nvim-ufo: polish code

--- lua/plugins/editing/nvim-ufo.lua
+++ lua/plugins/editing/nvim-ufo.lua
@@ -4,25 +4,10 @@ return {
     "kevinhwang91/promise-async",
   },
   config = function()
-    require("ufo").setup({
-      open_fold_hl_timeout = 0, -- highlight duration after opening a fold
-      close_fold_kinds_for_ft = {
-        default = { "imports", "comment" },
-      },
-      preview = {
-        win_config = {
-          border = { "", "─", "", "", "", "─", "", "" },
-          winhighlight = "Normal:Folded",
-          winblend = 0,
-        },
-        mappings = {
-          scrollU = "<C-u>",
-          scrollD = "<C-d>",
-          jumpTop = "[",
-          jumpBot = "]",
-        },
-      },
+    local ufo = require("ufo")
 
+    ufo.setup({
+      open_fold_hl_timeout = 0, -- highlight duration after opening a fold
       provider_selector = function()
         -- indent provider is more similar to vscode folding
         return { "treesitter", "indent" }
@@ -39,6 +24,7 @@ return {
         for _, chunk in ipairs(virtText) do
           local chunkText = chunk[1]
           local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+
           if targetWidth > curWidth + chunkWidth then
             table.insert(newVirtText, chunk)
           else
@@ -57,18 +43,18 @@ return {
         -- change 'MoreMsg' to another to change the color of the suffix
         -- table.insert(newVirtText, { suffix, "MoreMsg" })
         table.insert(newVirtText, { suffix, "Number" })
+
         return newVirtText
       end,
     })
 
     vim.api.nvim_set_hl(0, "Folded", { -- cursor line highlight
-      -- bg = "#4a5568",
       bg = "#3a4254",
     })
 
-    vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
-    vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "close all folds" })
-    vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds, { desc = "Open folds except kinds" })
-    vim.keymap.set("n", "zm", require("ufo").closeFoldsWith, { desc = "Close folds with" }) -- closeAllFolds == closeFoldsWith(0)
+    vim.keymap.set("n", "zr", ufo.openFoldsExceptKinds, { desc = "Open folds except kinds" })
+    vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
+    vim.keymap.set("n", "zm", ufo.closeFoldsWith, { desc = "Close folds with" }) -- closeAllFolds == closeFoldsWith(0)
+    vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "close all folds" })
   end,
 }
