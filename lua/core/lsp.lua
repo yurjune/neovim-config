add09c3721bc2674968f9722c2d48a265fd04a06
yurjune <yurjune@naver.com>
2025-10-09 19:54:51 +0900

    lsp: move ts-ls autocmd

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -58,45 +58,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
--- LSP config for ts-ls
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = lsp_group,
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-
-    if client and client.name == "ts_ls" then
-      local function organize_imports()
-        vim.lsp.buf.code_action({
-          context = {
-            only = { "source.organizeImports" },
-            diagnostics = {},
-          },
-          apply = true,
-        })
-        vim.notify("Organize imports triggered", vim.log.levels.INFO, { title = "LSP" })
-      end
-
-      local function remove_unused()
-        vim.lsp.buf.code_action({
-          context = {
-            ---@diagnostic disable-next-line: assign-type-mismatch
-            only = { "source.removeUnused.ts" },
-            diagnostics = {},
-          },
-          apply = true,
-        })
-        vim.notify("Remove unused triggered", vim.log.levels.INFO, { title = "LSP" })
-      end
-
-      vim.api.nvim_create_user_command("OrganizeImports", organize_imports, {})
-      vim.api.nvim_create_user_command("RemoveUnused", remove_unused, {})
-
-      vim.keymap.set("n", "<leader>oi", organize_imports, { desc = "Organize Imports", buffer = ev.buf })
-      vim.keymap.set("n", "<leader>ru", remove_unused, { desc = "Remove unused", buffer = ev.buf })
-    end
-  end,
-})
-
 -- Enable LLM-based inline completion
 vim.api.nvim_create_autocmd("LspAttach", {
   group = lsp_group,
