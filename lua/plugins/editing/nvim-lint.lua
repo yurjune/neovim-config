aeb4c42f7d16ddb7500d5b21b31fbf8740e1467d
yurjune <yurjune@naver.com>
2025-10-10 01:13:48 +0900

    nvim-lint: enable eslint on js

--- lua/plugins/editing/nvim-lint.lua
+++ lua/plugins/editing/nvim-lint.lua
@@ -10,7 +10,7 @@ return {
     local lint = require("lint")
 
     lint.linters_by_ft = {
-      -- javascript = { "eslint_d" },
+      javascript = { "eslint_d" },
       typescript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
