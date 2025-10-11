3ff591591dc1252e6cd54c42b4b1fc8fbf89d877
yurjune <yurjune@naver.com>
2025-10-11 18:20:07 +0900

    autocmd: enable r, o formatoptions to keep commenting

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd("FileType", {
     -- option for remove continue commenting when line changes
     -- r: continue commenting when line changed by enter
     -- o: continue commenting when line changed by o or O command
-    vim.opt_local.formatoptions:remove({ "o" })
+    vim.opt_local.formatoptions:append({ "r", "o" })
   end,
 })
 
