eab3fd542970b0fecace6226d609de118b72cbb3
yurjune <yurjune@naver.com>
2026-01-10 14:53:17 +0900

    render-markdown: config

--- lua/plugins/markdown/render-markdown.lua
+++ lua/plugins/markdown/render-markdown.lua
@@ -8,19 +8,40 @@ vim.cmd.packadd("render-markdown.nvim")
 local rm = require("render-markdown")
 require("render-markdown").setup({
   enabled = true,
+  debounce = 20, -- reduce debounce ms for proper render on fast current line move
   render_modes = { "n", "c", "t", "i", "v", "V" },
   completions = {
-    lsp = { enabled = true },
+    lsp = {
+      enabled = true,
+    },
+  },
+  latex = {
+    enabled = false,
   },
   heading = {
-    position = "inline",
+    position = "overlay",
+    -- icons = { "󰼏 ", "󰼐 ", "󰼑 ", "󰼒 ", "󰼓 ", "󰼔 " },
+    icons = { "█ ", "▓ ", "▒ ", "░ ", "▪ ", "·" },
   },
-  pipe_table = {
-    enabled = false,
-    preset = "none",
-    padding = 2,
+  code = {
+    enabled = true,
+    conceal_delimiters = false, -- conceal ``` text
+    -- Determines how the top / bottom of code block are rendered.
+    border = "thick",
+    width = "full", -- block | full
+    -- Turn on / off inline code related rendering.
+    inline = true,
+    -- Padding to add to the left & right of inline code.
+    -- inline_pad = 1, -- set padding 1 for ` char space
   },
-  latex = { enabled = false },
+  anti_conceal = {
+    -- This enables hiding added text on the line the cursor is on.
+    enabled = true,
+    disabled_modes = { "n " },
+    ignore = {
+      head_background = true,
+    },
+  },
 })
 
 vim.api.nvim_set_hl(0, "RenderMarkdownCode", {
