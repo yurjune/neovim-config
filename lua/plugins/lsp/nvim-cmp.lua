9d460ce086597562ba1b6083e598c87e7a63ad4c
yurjune <yurjune@naver.com>
2026-02-02 19:12:56 +0900

    cmp: fix no completion on command line in leetcode

--- lua/plugins/lsp/nvim-cmp.lua
+++ lua/plugins/lsp/nvim-cmp.lua
@@ -34,14 +34,11 @@ return {
         if vim.tbl_contains(exlcude_ft, filetype) then
           return false
         end
-        if vim.g.leetcode then
-          return false
-        end
         return true
       end,
 
       -- sources for autocompletion (order matters - higher priority first)
-      sources = cmp.config.sources({
+      sources = vim.g.leetcode and {} or cmp.config.sources({
         { name = "nvim_lsp" }, -- from cmp-nvim-lsp
         { name = "buffer" }, -- from cmp-buffer
         { name = "luasnip" }, -- from cmp_luasnip
