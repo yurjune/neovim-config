ea046eafb955223b021f5d74c3b08bb5b561c715
yhpark-gb <yhpark@gb-soft.co.kr>
2026-07-14 09:45:57 +0900

    supermaven: keep toggle state

--- lua/plugins/ai/supermaven.lua
+++ lua/plugins/ai/supermaven.lua
@@ -1,7 +1,7 @@
 -- A plugin for inline code completion
 return {
   "supermaven-inc/supermaven-nvim",
-  enabled = false,
+  enabled = true,
   event = "InsertEnter",
   cond = function()
     return not vim.g.leetcode
@@ -9,6 +9,19 @@ return {
   config = function()
     local sp = require("supermaven-nvim")
     local api = require("supermaven-nvim.api")
+    local state_file = vim.fn.stdpath("state") .. "/supermaven_enabled"
+
+    local function load_enabled()
+      return vim.uv.fs_stat(state_file) ~= nil
+    end
+
+    local function save_enabled(enabled)
+      if enabled then
+        vim.fn.writefile({ "1" }, state_file)
+      else
+        vim.fn.delete(state_file)
+      end
+    end
 
     sp.setup({
       keymaps = {
@@ -28,13 +41,25 @@ return {
       disable_keymaps = false, -- disables built in keymaps for more manual control
     })
 
+    -- restore previous state
+    if not load_enabled() and api.is_running() then
+      api.stop()
+    elseif load_enabled() and not api.is_running() then
+      api.start()
+    end
+
     vim.keymap.set("n", "<leader>cc", function()
-      if api.is_running() then
-        vim.notify("Supermaven disabled", vim.log.levels.INFO, { title = "Supermaven" })
-      else
+      local enabled = not api.is_running()
+
+      if enabled then
+        api.start()
         vim.notify("Supermaven enabled", vim.log.levels.INFO, { title = "Supermaven" })
+      else
+        api.stop()
+        vim.notify("Supermaven disabled", vim.log.levels.INFO, { title = "Supermaven" })
       end
-      api.toggle()
+
+      save_enabled(enabled)
     end, { desc = "Toggle Supermaven" })
   end,
 }
