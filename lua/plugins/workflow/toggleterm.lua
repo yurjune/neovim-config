8e801551f973c0774592328cc34d4f1a9d60ae19
yhpark-gb <yhpark@gb-soft.co.kr>
2026-07-21 13:11:02 +0900

    toggleterm: simplify config

--- lua/plugins/workflow/toggleterm.lua
+++ lua/plugins/workflow/toggleterm.lua
@@ -5,19 +5,12 @@ return {
   config = function()
     require("toggleterm").setup({
       size = 22,
-      -- 숫자 + 단축키로 여러 터미널 세션을 관리할 수 있다.
-      open_mapping = nil,
-      start_in_insert = true,
-      insert_mappings = true, -- whether or not the open mapping applies in insert mode
-      terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
-      auto_scroll = true, -- automatically scroll to the bottom on terminal output
       persist_size = true,
       persist_mode = false, -- if set to true (default) the previous terminal mode will be remembered
 
       direction = "float", -- 'vertical' | 'horizontal' | 'tab' | 'float'
-      -- This field is only relevant if direction is set to 'float'
       float_opts = {
-        border = "curved", -- 'single' | 'double' | 'shadow' | 'curved'
+        border = "curved",
         width = 180,
         height = 45,
       },
