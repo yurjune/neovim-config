8f0aadd68ddf82c1e01bf774b2a54f74fc0b6dc9
yhpark-gb <yhpark@gb-soft.co.kr>
2026-07-27 12:22:43 +0900

    input-source: replace im-select to hammerspoon

--- lua/etc/switch-input-on-insert.lua
+++ lua/etc/switch-input-on-insert.lua
@@ -1,51 +1,36 @@
 -- Switch input source when leave or enter insert mode
+-- Needs specific config in Hammerspoon
 
-local last_input_source = nil
-local en_input_source = "com.apple.keylayout.ABC"
+local hs_cli = "/usr/local/bin/hs"
+local hs_module = "nvimInputSource"
 
-local function get_input_source_async(callback)
-  vim.fn.jobstart("im-select", {
-    on_stdout = function(_, data)
-      if data and data[1] then
-        local im = vim.fn.trim(data[1])
-        callback(im)
-      end
-    end,
-    stdout_buffered = true,
+local function call_hammerspoon(method)
+  local command = hs_module .. "." .. method .. "()" -- ex) nvimInputSource.enterInsert()
+  vim.fn.jobstart({
+    hs_cli,
+    "-c",
+    command,
+  }, {
+    detach = true, -- prevent killing hs process when exit
   })
-end
-
-local function set_input_source_async(im)
-  if not im or im == "" then
-    return
-  end
-  vim.fn.jobstart("im-select " .. im)
 end
 
 local augroup = vim.api.nvim_create_augroup("SwitchInputOnInsert", { clear = true })
 
--- save current input source change change source to EN when leave insert mode
 vim.api.nvim_create_autocmd("InsertLeave", {
   group = augroup,
   pattern = "*",
+  desc = "Save input source and switch to English",
   callback = function()
-    get_input_source_async(function(im)
-      if im and im ~= "" then
-        last_input_source = im
-      end
-    end)
-
-    set_input_source_async(en_input_source)
+    call_hammerspoon("leaveInsert")
   end,
 })
 
--- restore last input source when enter insert mode
 vim.api.nvim_create_autocmd("InsertEnter", {
   group = augroup,
   pattern = "*",
+  desc = "Restore saved input source",
   callback = function()
-    if last_input_source and last_input_source ~= "" and last_input_source ~= en_input_source then
-      set_input_source_async(last_input_source)
-    end
+    call_hammerspoon("enterInsert")
   end,
 })
