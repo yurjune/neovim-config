1b94625774964aa4d8894e193b29ab20f08b75ad
yhpark-gb <yhpark@gb-soft.co.kr>
2026-07-30 14:44:43 +0900

    bufferline: protect pinned buffer from destroy

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -6,6 +6,7 @@ return {
   after = "catppuccin",
   config = function()
     local bufferline = require("bufferline")
+    local groups = require("bufferline.groups")
 
     -- NOTE: catppuccin dependency
     local highlights
@@ -37,16 +38,34 @@ return {
 
     local function close_buffer()
       local current_buf = vim.api.nvim_get_current_buf()
+      if groups._is_pinned({ id = current_buf }) then
+        vim.notify("Unpin the buffer before closing it.", vim.log.levels.WARN, { title = "bufferline.nvim" })
+        return
+      end
+
       vim.cmd("bnext")
       vim.api.nvim_buf_delete(current_buf, { force = false })
     end
     vim.keymap.set("n", "<M-w>", close_buffer, { desc = "Delete current buffer and move to next buffer" })
 
     local function close_all_buffers()
-      bufferline.close_others()
-      vim.notify("Cleared all buffers.", vim.log.levels.INFO)
+      local current_buf = vim.api.nvim_get_current_buf()
+      local to_close = {}
+
+      -- Keep both pinned buffers and the current buffer
+      for _, element in ipairs(bufferline.get_elements().elements) do
+        if element.id ~= current_buf and not groups._is_pinned(element) then
+          table.insert(to_close, element.id)
+        end
+      end
+
+      for _, bufnr in ipairs(to_close) do
+        vim.api.nvim_buf_delete(bufnr, { force = true })
+      end
+
+      vim.notify("Cleared all buffers.", vim.log.levels.INFO, { title = "bufferline.nvim" })
     end
-    vim.keymap.set("n", "<M-W>", close_all_buffers, { desc = "Clear buffers except current buffer and nvim-tree" })
+    vim.keymap.set("n", "<M-W>", close_all_buffers, { desc = "Clear buffers except current and pinned buffers" })
 
     vim.keymap.set("n", "<leader>bh", "<cmd>BufferLineMovePrev<CR>", { desc = "Move buffer backward" })
     vim.keymap.set("n", "<leader>bl", "<cmd>BufferLineMoveNext<CR>", { desc = "Move buffer forward" })
