a92de9c8c5a95366297ea570c4c6bb921b1139a4
yurjune <yurjune@naver.com>
2026-08-02 15:48:05 +0900

    blank-sidebar: add view project tree feature

--- lua/ui/blank-sidebar.lua
+++ lua/ui/blank-sidebar.lua
@@ -75,8 +75,102 @@ function M.toggle()
   end
 end
 
+function M.open_tree(opts)
+  opts = opts or {}
+  local buf = vim.api.nvim_win_get_buf(M.open())
+  local namespace = vim.api.nvim_create_namespace("blank-sidebar-project-tree")
+
+  local command = {
+    "env",
+    "LS_COLORS=di=1",
+    "tree",
+    "-C",
+  }
+
+  if opts.depth then
+    vim.list_extend(command, { "-L", tostring(opts.depth) })
+  end
+  if opts.hidden then
+    table.insert(command, "-a")
+  end
+  if opts.filter and #opts.filter > 0 then
+    vim.list_extend(command, { "-I", table.concat(opts.filter, "|") })
+  end
+
+  local output = vim.fn.systemlist(command)
+  local function parse_directory_line(line)
+    -- `tree -C` wraps directory names with ANSI color codes.
+    local prefix, name, suffix = line:match("^(.-)\27%[[%d;]*m(.-)\27%[[%d;]*m(.*)$")
+    if not name then
+      return line
+    end
+
+    return prefix .. name .. suffix, { #prefix, #prefix + #name }
+  end
+
+  local function highlight_directories(ranges)
+    vim.api.nvim_set_hl(0, "BlankSidebarDirectory", {
+      fg = "#94e2d5",
+      bold = true,
+    })
+    vim.api.nvim_buf_clear_namespace(buf, namespace, 0, -1)
+    for row, columns in pairs(ranges) do
+      vim.api.nvim_buf_set_extmark(buf, namespace, row - 1, columns[1], {
+        end_col = columns[2],
+        hl_group = "BlankSidebarDirectory",
+      })
+    end
+  end
+
+  local directory_ranges = {}
+  for row, line in ipairs(output) do
+    output[row], directory_ranges[row] = parse_directory_line(line)
+  end
+
+  vim.bo[buf].modifiable = true
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, output)
+  vim.bo[buf].modifiable = false
+  highlight_directories(directory_ranges)
+  vim.b[buf].tree_visible = true
+end
+
+function M.close_tree()
+  local sidebar = find_window()
+  if not sidebar then
+    return
+  end
+
+  local buf = vim.api.nvim_win_get_buf(sidebar)
+  local namespace = vim.api.nvim_create_namespace("blank-sidebar-project-tree")
+  vim.bo[buf].modifiable = true
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "" })
+  vim.bo[buf].modifiable = false
+  vim.api.nvim_buf_clear_namespace(buf, namespace, 0, -1)
+  vim.b[buf].tree_visible = false
+end
+
+function M.toggle_tree(opts)
+  local sidebar = find_window()
+  if sidebar and vim.b[vim.api.nvim_win_get_buf(sidebar)].tree_visible then
+    M.close_tree()
+  else
+    M.open_tree(opts)
+  end
+end
+
 function M.setup()
   vim.keymap.set("n", "<leader>bb", M.toggle, { desc = "Toggle blank sidebar" })
+
+  vim.keymap.set("n", "<leader>pt", function()
+    M.toggle_tree({
+      hidden = true,
+      filter = {
+        ".next",
+        "node_modules",
+        ".git",
+      },
+    })
+  end, { desc = "Toggle project tree" })
 end
 
 return M
