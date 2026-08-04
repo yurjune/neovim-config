2ec82a6e64ed220a904da87f97eec4ee937af51e
yhpark-gb <yhpark@gb-soft.co.kr>
2026-08-04 17:03:47 +0900

    zen-mode: install package

---
+++ lua/plugins/ui/zen-mode.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/zen-mode.nvim",
+  keys = {
+    { "<leader>wn", "<cmd>ZenMode<CR>", desc = "Toggle Zen mode" },
+  },
+  opts = {
+    window = {
+      width = 120,
+    },
+  },
+}
