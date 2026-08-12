c71e069e3111c943c1cb413792a4c23b92c3cd48
yurjune <yurjune@naver.com>
2026-08-12 16:55:04 +0900

    refactor(fzf): change keymaps

--- lua/plugins/navigation/fzf-lua.lua
+++ lua/plugins/navigation/fzf-lua.lua
@@ -77,8 +77,9 @@ return {
 
     vim.keymap.set("n", "<leader>fh", fzf.files, { desc = "FzfLua find files" })
     vim.keymap.set("n", "<leader>fH", function()
-      fzf.files({ query = vim.fn.expand("%:t:r") })
-    end, { desc = "FzfLua find files matching current filename" })
+      local dir = vim.fn.expand("%:.:h")
+      fzf.files({ query = dir == "." and "" or dir .. "/" })
+    end, { desc = "FzfLua find files in current file directory" })
     vim.keymap.set("n", "<leader>fg", fzf.git_status, { desc = "FzfLua git status" })
 
     vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "FzfLua buffers" })
