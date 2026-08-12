c774a07ecf1e45866a46152b099b70e90768d4dd
yurjune <yurjune@naver.com>
2026-08-12 16:55:03 +0900

    oil: install package

---
+++ lua/plugins/navigation/oil.lua
@@ -0,0 +1,38 @@
+-- A file explorer that lets you edit directories like a normal buffer
+return {
+  "stevearc/oil.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+    { "<leader>be", "<cmd>Oil<CR>", desc = "Open Oil" },
+  },
+  opts = {
+    default_file_explorer = false,
+    keymaps = {
+      ["g?"] = { "actions.show_help", mode = "n" },
+      ["<CR>"] = "actions.select",
+      ["<Tab>"] = "actions.select",
+      ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+      ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+      ["<C-t>"] = { "actions.select", opts = { tab = true } },
+      -- ["<C-p>"] = "actions.preview",
+      ["<C-c>"] = { "actions.close", mode = "n" },
+      ["<C-l>"] = "actions.refresh",
+      ["-"] = { "actions.parent", mode = "n" },
+      ["q"] = { "actions.parent", mode = "n" },
+      ["_"] = { "actions.open_cwd", mode = "n" },
+      ["`"] = { "actions.cd", mode = "n" },
+      ["g~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+      ["gs"] = { "actions.change_sort", mode = "n" },
+      ["gx"] = "actions.open_external",
+      ["g."] = { "actions.toggle_hidden", mode = "n" },
+      ["g\\"] = { "actions.toggle_trash", mode = "n" },
+    },
+    lsp_file_methods = {
+      enabled = true,
+      timeout_ms = 1000,
+      autosave_changes = true,
+    },
+  },
+}
