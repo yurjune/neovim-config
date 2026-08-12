42ea9c4718d9c7c12807fee12dada1e768a957d9
yurjune <yurjune@naver.com>
2026-08-12 16:55:03 +0900

    noice: polish config

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -1,54 +1,45 @@
 -- A plugin that replaces the UI for messages, cmdline and the popupmenu.
 return {
   "folke/noice.nvim",
+  enabled = true,
   dependencies = {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
-  -- presets > lsp_doc_border 가 빈번하게 적용되지 않는 경우가 있어 priority 를 높여 우선적으로 로드.
   priority = 900,
   opts = {
     cmdline = {
       enabled = true,
-      view = "cmdline_popup", -- cmdline, cmdline_popup
-      format = {
-        search_down = { icon = "v" },
-        search_up = { icon = "^" },
-      },
+      view = "cmdline_popup", -- floating popup
     },
-    -- 전통적인 vim 의 시스템 메시지
+    -- traditional vim's system messages
     messages = {
       enabled = true,
-      view = "notify", -- notify, mini, virtualtext, popup, messages, split
-      view_error = "notify", -- view for errors
+      view = "notify",
+      view_error = "notify",
     },
-    -- vim.notify 함수를 통해 전송되는 알림을 처리
+    -- notifications caused by "vim.notify"
     notify = {
       enabled = true,
-      view = "notify", -- notify, mini, split, popup, hover
+      view = "notify",
     },
+    -- cmdline autocomplete popup
     popupmenu = {
       enabled = true,
-      backend = "nui", -- backend to use to show regular cmdline completions (cmp or nui)
+      backend = "nui", -- cmp or nui
       kind_icons = {}, -- set to `false` to disable icons
     },
     lsp = {
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      -- override markdown rendering so that cmp and other plugins use Treesitter
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
         ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
       },
+      -- vim.lsp.buf.hover()
       hover = {
         enabled = true,
         silent = true, -- suppress "No information available" notifications
-      },
-    },
-    views = {
-      cmdline_popup = {
-        size = {
-          height = 1, -- line numbers
-        },
       },
     },
     presets = {
