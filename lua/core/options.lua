a87a1961da018c38d42ba94409ff20a1b50e9091
yurjune <yurjune@naver.com>
2026-08-12 16:55:03 +0900

    options: sessionoptions: string to table

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -32,7 +32,19 @@ vim.opt.foldlevel = 99
 
 -- Decide what inormation to save when saving a session
 -- localoptions: local options set for each window or buffer
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions,globals"
+vim.opt.sessionoptions = {
+  "blank", -- Save empty windows
+  "buffers", -- Save all buffers, including hidden ones
+  "curdir", -- Save the current working directory
+  "folds", -- Save fold states
+  "help", -- Save help windows
+  "tabpages", -- Save all tab pages
+  "winsize", -- Save window sizes
+  "winpos", -- Save the GUI window position
+  "terminal", -- Save terminal windows
+  "localoptions", -- Save buffer-local and window-local options
+  "globals", -- Save eligible global variables
+}
 
 -- cursor
 vim.opt.cursorline = true -- highlight the current cursor line
